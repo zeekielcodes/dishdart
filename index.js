2ebbfade4613421cff66da92ea7cc6c401cb2aa4
unknown <ezekielabolade@gmail.com>
2024-01-19 11:31:25 +0100

    Testing again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 // const express = require("express");
 // const cors = require("cors");
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
 app.use(cors());
